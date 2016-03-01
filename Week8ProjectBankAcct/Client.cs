f92c5e2cc83351d8f3878fceb19a756ddcc3742d
Mary Winkelman <marymwinkelman@gmail.com>
2016-03-01 17:48:24 -0500

    setup commit

---
+++ Week8ProjectBankAcct/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8ProjectBankAcct
+{
+    class Client
+    {
+        private string firstName;
+        private string lastName;
+        private Random acctNumber;
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+        public int AcctNumber
+        {
+            get { return acctNumber; }
+            set { acctNumber = value; }
+        }
+
+        public Client()
+        {
+            firstName = null;
+            lastName = null;
+            acctNumber = null;
+        }
+        public Client(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.acctNumber = new Random();
+
+            Console.WriteLine($"Opened an account for {this.firstName} {this.lastName} with account number {this.acctNumber}.");
+        }
+    }
+}
---
+++ Week8ProjectBankAcct/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8ProjectBankAcct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> menuItems = new List<string>
+            { "View Client Information","View Account Balance","Deposit Funds","Withdraw Funds","Exit" };
+
+     
+       
+            DisplayMenu(menuItems);
+            string menuChoice = Console.ReadLine();
+            
+            switch(menuChoice)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+            } 
+            Console.Read();
+
+        }
+        static void DisplayMenu(List<string> menuItems)
+        {
+
+            Console.WriteLine("Personal Banking Program \n-------------------------\nMENU:");
+            int counter = 0;
+            foreach (string item in menuItems)
+            {
+                counter++;
+                Console.WriteLine($"{counter}. {item}");
+            }
+        }
+    }
+}
